78e25ed26514540523f08cf7e61ad112174921a7
Avishka <avishkakapuruge@gmail.com>
2025-01-13 22:23:25 +0530

    update: react

--- ecomm/container/src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import { mount as productsMounts } from "products/ProductsIndex";
-import { mount as cartMounts } from "cart/CartShow";
-
-console.log('Container');
-
-productsMounts(document.querySelector("#my-products"));
-cartMounts(document.querySelector("#my-cart"));
